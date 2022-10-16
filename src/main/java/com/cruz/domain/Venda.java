40acb8cb1b0eb22129729f1c81610ad447d76c4c
Matheus Cruz <matheusd20m@gmail.com>
2022-10-16 20:34:43 -0300

    Conexao com DB H2 e Criacao automatica da dos dados

--- src/main/java/com/cruz/domain/Venda.java
+++ src/main/java/com/cruz/domain/Venda.java
@@ -5,11 +5,24 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
 public class Venda implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@CreationTimestamp
+	@Column(updatable = false)
 	private LocalDateTime dataDaVenda;
 
 	private BigDecimal valor;
