c89455886ee0d15fc9a1857450b8cf18e82366a8
Matheus Cruz <matheusd20m@gmail.com>
2022-10-16 20:07:16 -0300

    Criacao do projeto

---
+++ src/test/java/com/cruz/SistemaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
